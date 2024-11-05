ace353d528457b13684e5cc6567c084c3d39b55c
Mike Linder <miky2493@gmail.com>
2024-11-05 15:55:15 -0300

    Testing...

--- src/components/main/main-content/Main-content-2.tsx
+++ src/components/main/main-content/Main-content-2.tsx
@@ -10,6 +10,7 @@ export function MainContent_2() {
             <p className="main">
                 Preparado(a) para explorar tudo o que a tecnologia tem de melhor, de maneira fácil e prática?
             </p>
+            <span id="img-smartphone"></span>
             <p className="main">
                 Aqui, você aprenderá a dominar seu smartphone, se conectar com a família via WhatsApp, curtir vídeos no YouTube, e até mesmo fazer compras e pagar contas online com total segurança. Então vamos ao que interessa...
             </p>
